077453624a40803524beb2d22c2a365992e2ac3d
Joe Webb <webbdeveloping@gmail.com>
2020-02-25 00:13:38 -0700

    finished side area

--- src/App.js
+++ src/App.js
@@ -43,9 +43,106 @@ function App() {
           </div>
         </nav>
       </header>
+
       <div className='content'>
-        <nav className='sidebar'>Navigation</nav>
-        <main className='hotel-view'>Hotel View</main>
+        <nav className='sidebar'>
+          <ul className='side-nav'>
+            <li className='side-nav__item side-nav__item--active'>
+              <a href='' className='side-nav__link'>
+                <svg className='side-nav__icon'>
+                  <use xlinkHref={'img/sprite.svg#icon-home'}></use>
+                </svg>
+                <span>Hotel</span>
+              </a>
+            </li>
+
+            <li className='side-nav__item'>
+              <a href='' className='side-nav__link'>
+                <svg className='side-nav__icon'>
+                  <use
+                    xlinkHref={'img/sprite.svg#icon-aircraft-take-off'}
+                  ></use>
+                </svg>
+                <span>Flight</span>
+              </a>
+            </li>
+
+            <li className='side-nav__item'>
+              <a href='' className='side-nav__link'>
+                <svg className='side-nav__icon'>
+                  <use xlinkHref={'img/sprite.svg#icon-key'}></use>
+                </svg>
+                <span>Car Rental</span>
+              </a>
+            </li>
+
+            <li className='side-nav__item'>
+              <a href='' className='side-nav__link'>
+                <svg className='side-nav__icon'>
+                  <use xlinkHref={'img/sprite.svg#icon-map'}></use>
+                </svg>
+                <span>Tours</span>
+              </a>
+            </li>
+          </ul>
+          <div className='legal'>
+            &copy; 2020 by Trillo All rights reserved.
+          </div>
+        </nav>
+
+        <main className='hotel-view'>
+          <div className='gallery'>
+            <figure className='gallery__item'>
+              <img
+                src={'../img/hotel-1.jpg'}
+                alt='Photo of hotel 1'
+                className='gallery__photo'
+              />
+            </figure>
+            <figure className='gallery__item'>
+              <img
+                src={'../img/hotel-2.jpg'}
+                alt='Photo of hotel 1'
+                className='gallery__photo'
+              />
+            </figure>
+            <figure className='gallery__item'>
+              <img
+                src={'../img/hotel-3.jpg'}
+                alt='Photo of hotel 1'
+                className='gallery__photo'
+              />
+            </figure>
+          </div>
+          <div className='overview'>
+            <h1 className='overview__heading'>Hotel Las Palms</h1>
+            <div className='overview__stars'>
+              <svg className='overview__icon--star'>
+                <use xlinkHref={'img/sprite.svg#icon-star'}></use>
+              </svg>
+              <svg className='overview__icon--star'>
+                <use xlinkHref={'img/sprite.svg#icon-star'}></use>
+              </svg>
+              <svg className='overview__icon--star'>
+                <use xlinkHref={'img/sprite.svg#icon-star'}></use>
+              </svg>
+              <svg className='overview__icon--star'>
+                <use xlinkHref={'img/sprite.svg#icon-star'}></use>
+              </svg>
+              <svg className='overview__icon--star'>
+                <use xlinkHref={'img/sprite.svg#icon-star'}></use>
+              </svg>
+            </div>
+            <div className='overview__icon--location'>
+              <svg className='side-nav__icon'>
+                <use xlinkHref={'img/sprite.svg#icon-location-pin'}></use>
+                <button className='btn-inline'>Utah, Portugal</button>
+              </svg>
+            </div>
+            <div className='overview__rating'>8.6</div>
+            <div className='overview_rating-count'>420.9</div>
+          </div>
+        </main>
       </div>
     </div>
   );
